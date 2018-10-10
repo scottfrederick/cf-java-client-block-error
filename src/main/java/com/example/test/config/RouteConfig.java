5184e3ce62e2877426764328ac6b17d34ea50a0e
Scott Frederick <scottyfred@gmail.com>
2018-10-10 14:00:50 -0500

    Initial commit

---
+++ src/main/java/com/example/test/config/RouteConfig.java
@@ -0,0 +1,20 @@
+package com.example.test.config;
+
+import com.example.test.web.InfoHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+@Configuration
+public class RouteConfig {
+	@Bean
+	public RouterFunction<ServerResponse> route(InfoHandler greetingHandler) {
+		return RouterFunctions
+				.route(RequestPredicates.GET("/spaces")
+						.and(RequestPredicates.accept(MediaType.TEXT_PLAIN)), greetingHandler::spaces);
+	}
+}
---
+++ src/main/java/com/example/test/web/InfoHandler.java
@@ -0,0 +1,48 @@
+package com.example.test.web;
+
+import org.cloudfoundry.operations.CloudFoundryOperations;
+import org.cloudfoundry.operations.spaces.CreateSpaceRequest;
+import org.cloudfoundry.operations.spaces.SpaceSummary;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+import reactor.util.Logger;
+import reactor.util.Loggers;
+
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Component
+public class InfoHandler {
+	private final Logger log = Loggers.getLogger(InfoHandler.class);
+	private final CloudFoundryOperations operations;
+
+	public InfoHandler(CloudFoundryOperations operations) {
+		this.operations = operations;
+	}
+
+	@SuppressWarnings("unused")
+	public Mono<ServerResponse> spaces(ServerRequest request) {
+		return ServerResponse.ok()
+				.contentType(MediaType.TEXT_PLAIN)
+				.body(BodyInserters.fromPublisher(createSpace().then(getSpaceNames()), String.class));
+	}
+
+	private Mono<Void> createSpace() {
+		return this.operations.spaces()
+				.create(CreateSpaceRequest.builder()
+						.name(UUID.randomUUID().toString())
+						.build());
+	}
+
+	private Mono<String> getSpaceNames() {
+		return this.operations.spaces()
+				.list()
+				.map(SpaceSummary::getName)
+				.doOnNext(spaceName -> log.info("found space '{}'", spaceName))
+				.collect(Collectors.joining(","));
+	}
+}
